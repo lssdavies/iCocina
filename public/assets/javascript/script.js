a47fddb8c348fe208ddca3410b177b89fe385679
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-01-20 18:38:40 -0800

    made design changes added some JavaScript functionality

---
+++ public/assets/javascript/script.js
@@ -0,0 +1,18 @@
+const loginBtn = document.getElementById('signIn');
+const signUpBtn = document.getElementById('signUp');
+function showLogin()   {
+    //hide login and sign up buttons on page by adding class to hide element.
+
+    // display hidden form input elements for email address and password login button
+
+
+
+}
+
+
+
+
+
+
+loginBtn.addEventListener('click', showLogin);
+signUpBtn.addEventListener('click', showLogin);
